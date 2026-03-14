87407e29a6fe6043b9e3e31927eba5e26a37a697
Moritz Brödel <moritz.broedel@gmail.com>
2026-03-14 15:26:40 +0100

    Update for 0.3.1

--- Casks/groupctrl.rb
+++ Casks/groupctrl.rb
@@ -1,7 +1,7 @@
 cask "groupctrl" do
-  version "0.3.0"
-  sha256 arm:   "04e7d9d22b9cf6535510c28cdeb073644dcee8ef8781070946a4b4d1744b13d4",
-         intel: "fc85df65bf6f9e15044d3e9d2ff314cc36a3851567b384b96e1428293aaae882"
+  version "0.3.1"
+  sha256 arm:   "b657fafe610cacedc2071ba86431f58b87d5cb989c1aecb4a95a3d4ccb29f7dd",
+         intel: "97c2d9fc849b5e9486a568df1e6956cfea4c45375447f8eba08341d50192ec2f"
 
   arch arm: "Arm", intel: "Intel"
   url "https://github.com/brodmo-dev/GroupCtrl/releases/download/v#{version}/GroupCtrl-#{version}-#{arch}.dmg"
