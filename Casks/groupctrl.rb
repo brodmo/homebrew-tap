82e963869764a82d20f6be1b53b6e2aec79b64b7
Moritz Brödel <moritz.broedel@gmail.com>
2026-04-05 00:21:04 +0200

    Update repo URL

--- Casks/groupctrl.rb
+++ Casks/groupctrl.rb
@@ -4,10 +4,10 @@ cask "groupctrl" do
          intel: "b1d9382d784fd841d33c15c59647799fd931b6fecb7a9bbc4b1a213f0f299cba"
 
   arch arm: "Arm", intel: "Intel"
-  url "https://github.com/brodmo-dev/GroupCtrl/releases/download/v#{version}/GroupCtrl-#{version}-#{arch}.dmg"
+  url "https://github.com/brodmo/GroupCtrl/releases/download/v#{version}/GroupCtrl-#{version}-#{arch}.dmg"
   name "GroupCtrl"
   desc "Instant app switching with shared hotkeys"
-  homepage "https://github.com/brodmo-dev/GroupCtrl"
+  homepage "https://github.com/brodmo/GroupCtrl"
 
   livecheck do
     url :url
